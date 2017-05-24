520e7354bca6507b84ad21336de4fcdff83bdb27
Conor <office@conorryan.ie>
2017-05-24 17:27:46 +0100

    update bucket info on main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,12 @@
 variable "region" {
-  default = "eu-west-1b"
+  default = "eu-west-1"
 }
 
 variable "domain_name" {
+  default = "https://s3-eu-west-1.amazonaws.com/conortek.net/"
+}
+
+variable "bucket_name" {
   default = "https://s3-eu-west-1.amazonaws.com/conortek.net/"
 }
 
@@ -10,8 +14,8 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_s3_bucket" "site" {
-  bucket = "${var.domain_name}"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.bucket_name}"
   region = "${var.region}"
   acl = "public-read"
   website {
@@ -26,76 +30,8 @@ resource "aws_s3_bucket" "site" {
     "Effect": "Allow",
     "Principal": "*",
     "Action": "s3:GetObject",
-    "Resource": ["arn:aws:s3:::${var.domain_name}/*"]
+    "Resource": ["arn:aws:s3:::${var.bucket_name}/*"]
   }]
 }
 EOF
-}
-
-resource "aws_s3_bucket" "wwwsite" {
-  bucket = "www.${var.domain_name}"
-  region = "${var.region}"
-  acl = "public-read"
-  website {
-    redirect_all_requests_to = "${var.domain_name}"
-  }
-}
-
-resource "aws_s3_bucket_object" "index_file" {
-  bucket = "${var.domain_name}"
-  source = "index.html"
-  key = "index.html"
-  etag = "${md5(file("index.html"))}"
-  content_type = "text/html"
-  depends_on = [
-    "aws_s3_bucket.site"
-  ]
-}
-
-resource "aws_s3_bucket_object" "error_file" {
-  bucket = "${var.domain_name}"
-  source = "error.html"
-  key = "error.html"
-  etag = "${md5(file("error.html"))}"
-  content_type = "text/html"
-  depends_on = [
-    "aws_s3_bucket.site"
-  ]
-}
-
-resource "aws_s3_bucket_object" "css_file" {
-  bucket = "${var.domain_name}"
-  source = "assets/stylesheets/style.css"
-  key = "assets/stylesheets/style.css"
-  etag = "${md5(file("assets/stylesheets/style.css"))}"
-  content_type = "text/css"
-  depends_on = [
-    "aws_s3_bucket.site"
-  ]
-}
-
-resource "aws_s3_bucket_object" "image_file" {
-  bucket = "${var.domain_name}"
-  source = "assets/images/image.jpeg"
-  key = "assets/images/image.jpeg"
-  etag = "${md5(file("assets/images/image.jpeg"))}"
-  content_type = "image/jpeg"
-  depends_on = [
-    "aws_s3_bucket.site"
-  ]
-}
-
-resource "aws_route53_zone" "primary" {
-  name = "${var.domain_name}"
-}
-
-resource "aws_route53_record" "site" {
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-  name = "${var.domain_name}"
-  type = "A"
-  alias {
-    name = "${aws_s3_bucket.site.website_domain}"
-    zone_id = "${aws_s3_bucket.site.hosted_zone_id}"
-    evaluate_target_health = false
-  }
 }
